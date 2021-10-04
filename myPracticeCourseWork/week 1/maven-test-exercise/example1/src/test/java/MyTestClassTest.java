60aa03f8a6536c1e0c0c99b2788c5cf7477f5cd7
elsa1q <5CAMAE35@solent.ac.uk>
2021-10-04 16:55:25 +0100

    commit of maven

---
+++ myPracticeCourseWork/week 1/maven-test-exercise/example1/src/test/java/MyTestClassTest.java
@@ -0,0 +1,23 @@
+
+import elsa1q.com504.ood.MyTestClassLog4j;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ppp
+ */
+public class MyTestClassTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        MyTestClassLog4j myTest = new MyTestClassLog4j();
+        myTest.writeAboutMe();
+    }
+
+}
